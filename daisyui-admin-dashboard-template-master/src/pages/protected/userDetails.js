7048189436e3ff87ed35fe097928f94834cd499d
James Adogo Ewoenam <jamesewoenam7@gmail.com>
2025-04-08 12:23:41 +0000

    Update

--- daisyui-admin-dashboard-template-master/src/pages/protected/userDetails.js
+++ daisyui-admin-dashboard-template-master/src/pages/protected/userDetails.js
@@ -75,6 +75,9 @@ function UserDetails() {
       </div>
       <div className="mt-6 p-4  rounded-xl">
         <h2 className="text-xl font-semibold mb-2 text-gray-700">Paid</h2>
+            <span className="text-gray-700 font-medium">Course Title</span>
+            <span className="text-blue-600 font-semibold">Cost</span>
+            <span className="text-blue-600 font-semibold">Grade</span>
         {userDetails.Paid.map((item,index) => (
           <div key={index} className="flex justify-between p-3 rounded-lg shadow-sm mb-2">
             <span className="text-gray-700 font-medium">{item.name}</span>
